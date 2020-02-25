86cadb1bb903ebada6f80495aee6cf1e1e697600
alberslash10 <albeferodriguez@gmail.com>
2020-02-25 20:00:47 +0100

    Mini project with basic activity and MVC

---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.geoquiz
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var question: TextView
+    private lateinit var leftButton: ImageButton
+    private lateinit var rightButton: ImageButton
+
+    private val questionList = listOf(
+        Question(R.string.question_Australia, true),
+        Question(R.string.question_Franica, false),
+        Question(R.string.question_Norway, true),
+        Question(R.string.question_Russia, true),
+        Question(R.string.question_Spain, false)
+    )
+
+    private var currentIndex = 0
+    private var prevIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById<Button>(R.id.true_button)
+        falseButton = findViewById<Button>(R.id.false_button)
+        question = findViewById<TextView>(R.id.question)
+
+        leftButton.setOnClickListener{
+            getPrevQuestion()
+        }
+
+        trueButton.setOnClickListener {
+            CheckAnswer(true)
+        }
+
+        falseButton.setOnClickListener {
+            CheckAnswer(false)
+        }
+
+        rightButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionList.size
+            UpdateQuestion()
+        }
+        UpdateQuestion()
+    }
+
+    fun UpdateQuestion() {
+        val questionTextResId = questionList[currentIndex].question
+        question.setText(questionTextResId)
+        currentIndex++
+    }
+
+    fun getPrevQuestion(){
+        if(currentIndex - 1 >  0) {
+            val questionResId = questionList[currentIndex - 1].question
+            question.setText(questionResId)
+        }
+        currentIndex -= 1
+    }
+
+    fun CheckAnswer(userAnswer: Boolean) {
+
+        val correctAnswer = questionList[currentIndex].correct
+
+        if (userAnswer == correctAnswer)
+            Toast.makeText(this, "Correct", Toast.LENGTH_LONG).show()
+        else
+            Toast.makeText(this, "False", Toast.LENGTH_LONG).show()
+
+    }
+}
---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/Question.kt
@@ -0,0 +1,3 @@
+package com.example.geoquiz
+
+class Question(val question: Int, val correct: Boolean)
