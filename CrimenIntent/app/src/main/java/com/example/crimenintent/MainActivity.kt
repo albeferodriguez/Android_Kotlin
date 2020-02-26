23520286d469e04643c9a5d0afee34ff818bac8f
alberslash10 <albeferodriguez@gmail.com>
2020-02-26 18:50:25 +0100

    Linked the fragment with his activity parent

--- CrimenIntent/app/src/main/java/com/example/crimenintent/MainActivity.kt
+++ CrimenIntent/app/src/main/java/com/example/crimenintent/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.crimenintent
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null){
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
